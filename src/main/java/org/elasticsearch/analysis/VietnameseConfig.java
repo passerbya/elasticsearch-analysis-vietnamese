ee99cd1d3b301c678fe30ad0aa6a3feb38efcd3c
passerbya <hanghang3103@163.com>
2021-10-02 13:50:23 +0800

    using coccoc-tokenizer.properties to config dict_path

--- src/main/java/org/elasticsearch/analysis/VietnameseConfig.java
+++ src/main/java/org/elasticsearch/analysis/VietnameseConfig.java
@@ -1,6 +1,11 @@
 package org.elasticsearch.analysis;
 
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugin.analysis.vi.AnalysisVietnamesePlugin;
 
 public class VietnameseConfig {
     public static final String DEFAULT_DICT_PATH = "/usr/local/share/tokenizer/dicts";
@@ -8,10 +13,19 @@ public class VietnameseConfig {
     public final Boolean keepPunctuation;
     public final Boolean splitHost;
     public final Boolean splitURL;
+    private static final String pluginBase = AnalysisVietnamesePlugin.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+    private static final Path safePath = Paths.get(new File(pluginBase).getParentFile().getAbsoluteFile().toURI());
 
 
     public VietnameseConfig(Settings settings) {
-        dictPath = settings.get("dict_path", DEFAULT_DICT_PATH);
+        dictPath = settings.get("dict_path");
+        if (dictPath == null ) {
+            Properties configPro = new Properties();
+            try (FileInputStream is = new FileInputStream(safePath.resolve("coccoc-tokenizer.properties").toFile())) {
+                configPro.load(is);
+                dictPath = configPro.get("dict_path", DEFAULT_DICT_PATH);
+            }
+        }
         keepPunctuation = settings.getAsBoolean("keep_punctuation", Boolean.FALSE);
         splitHost = settings.getAsBoolean("split_host", Boolean.FALSE);
         splitURL = settings.getAsBoolean("split_url", Boolean.FALSE);
